f616d7ba1200b99934bec3e9289435e5df7a11ad
Aadhil Syed Ibrahim <40351561+Aadhils@users.noreply.github.com>
2026-08-30 00:46:24 +0530

    phase13: add provider verification page

---
+++ web/app/provider/verification/page.tsx
@@ -0,0 +1,5 @@
+import ProviderVerificationManager from '../../../components/provider/ProviderVerificationManager';
+
+export default function ProviderVerificationPage() {
+  return <ProviderVerificationManager />;
+}
