831e3fdc84add58fccda3ea23af1b9911ff85c8f
Aadhil Syed Ibrahim <40351561+Aadhils@users.noreply.github.com>
2026-08-30 14:23:47 +0530

    Phase 16: add provider leads page

---
+++ web/app/provider/leads/page.tsx
@@ -0,0 +1,5 @@
+import { ProviderRequirementLeadsManager } from '../../../components/provider/ProviderRequirementLeadsManager';
+
+export default function ProviderLeadsRoute() {
+  return <ProviderRequirementLeadsManager />;
+}
