6a6712d51346dc06dbccfed4dca386d4d3864998
Aadhil Syed Ibrahim <40351561+Aadhils@users.noreply.github.com>
2026-09-05 04:28:22 +0530

    Use polished provider jobs experience

--- web/app/provider/jobs/page.tsx
+++ web/app/provider/jobs/page.tsx
@@ -1,12 +1,8 @@
 import { LiveProviderShell } from '../../../components/provider/LiveProviderShell';
-import { EmployerApplicantResumeReview } from '../../../components/jobs/EmployerApplicantResumeReview';
-import { JobOfferWorkspace } from '../../../components/jobs/JobOfferWorkspace';
-import { ProviderJobMarketplace } from '../../../components/jobs/ProviderJobMarketplace';
+import { ProviderJobsExperience } from '../../../components/jobs/ProviderJobsExperience';
 
 export default function ProviderJobsPage() {
   return <LiveProviderShell active="/provider/jobs">
-    <ProviderJobMarketplace />
-    <JobOfferWorkspace />
-    <EmployerApplicantResumeReview />
+    <ProviderJobsExperience />
   </LiveProviderShell>;
 }
