5c0e0793ebb466f4345db06e3b970e7cf706b829
Aadhil Syed Ibrahim <40351561+Aadhils@users.noreply.github.com>
2026-09-08 09:43:54 +0530

    Marketplace: surface Business product catalog from dashboard

--- web/components/provider/ProviderDashboardEntry.tsx
+++ web/components/provider/ProviderDashboardEntry.tsx
@@ -2,10 +2,12 @@
 
 import ProviderDashboardManager from './ProviderDashboardManager';
 import ProviderLiveAvailabilityControl from './ProviderLiveAvailabilityControl';
+import BusinessProductCatalogShortcut from './BusinessProductCatalogShortcut';
 
 export default function ProviderDashboardEntry() {
   return <>
     <ProviderDashboardManager />
+    <BusinessProductCatalogShortcut />
     <ProviderLiveAvailabilityControl />
   </>;
 }
