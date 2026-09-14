0b32f314cf67c9bcd5671ece1331b65935dcdb3c
Aadhil Syed Ibrahim <40351561+Aadhils@users.noreply.github.com>
2026-09-14 22:24:34 +0530

    UI: wrap Provider Profile for responsive polish

--- web/app/provider/profile/page.tsx
+++ web/app/provider/profile/page.tsx
@@ -1,5 +1,6 @@
 import ProviderProfileManager from '../../../components/provider/ProviderProfileManager';
+import styles from '../../../components/provider/ProviderProfileResponsive.module.css';
 
 export default function ProviderProfileRoute() {
-  return <ProviderProfileManager />;
+  return <div className={styles.profileJourney}><ProviderProfileManager /></div>;
 }
